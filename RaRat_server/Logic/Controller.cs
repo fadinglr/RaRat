5f5c386eeae2e3bc6c0283108abc6d68f7d84da6
Hosam Gemei <5104207+Gemei@users.noreply.github.com>
2020-07-16 16:30:05 +0200

    Commnucations module added
    - Socket client and server added
    - Changed .net target to 4.7.*

---
+++ RaRat_server/Logic/Controller.cs
@@ -0,0 +1,35 @@
+﻿using RaRat_server.Commnucation;
+using System;
+using System.Threading;
+
+namespace RaRat_server
+{
+    class Controller
+    {
+        public Controller() {
+            //sendCommand("127.0.0.1", 8080, "Test");
+            //startListening();
+            SocketServer.StartListening();
+        }
+
+        void sendCommand(string IP, int port, string command)
+        {
+            Thread socketClientThread = new Thread(()=>initSocketClient(IP, port, command));
+            socketClientThread.Start();
+        }
+        void startListening() {
+            Thread socketClientThread = new Thread(() => initSocketServer());
+            socketClientThread.Start();
+        }
+
+        void initSocketClient(string IP, int port, string command) {
+            SocketClient socketClient = new SocketClient();
+            socketClient.startClient(IP, port, command);
+            Console.WriteLine(socketClient.getResponse());
+        }
+        void initSocketServer()
+        {
+            SocketServer.StartListening();
+        }
+    }
+}
